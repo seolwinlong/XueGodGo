a97ac172b5f82cb7eb108ae9721db9a9cf103c40
winlong <winlong@outlook.com>
2025-02-11 22:16:48 +0800

    2025-02-11 22:16

--- Stage01_Basic/main.go
+++ Stage01_Basic/main.go
@@ -3,13 +3,13 @@ package main
 //包的调用：嗲用包里面的函数或变量需要遵循以下规则：
 //函数或变量首字母大写表示可以被外部调用，否则只能在包内部调用
 import (
-	"Stage01_Basic/eat"
+	eating "Stage01_Basic/eat"
 	test2 "Stage01_Basic/test"
 	"fmt"
 )
 
 func main() {
-	res := eat.Eat("面包")
+	res := eating.Eat("面包")
 	fmt.Println(res)
 
 	//Str1首字母大写可以被调用
